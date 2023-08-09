3cdba154e809260542a64e0a8441240d9062bd07
Thiago Ribeiro <thiagor21@gmail.com>
2023-08-09 19:29:11 -0300

    Versão validada e comentada dos scripts [WebScraping_OpenStreetMap.py] e [WebScraping_OpenStreetMap.py]

--- Rename_Move_Files.py
+++ Rename_Move_Files.py
@@ -1,20 +1,35 @@
+# Linha 3: Importa o módulo 'os', que fornece funções para interagir com o sistema operacional.
+# Linha 4: Importa o módulo 'shutil', que fornece funções para operações de cópia/movimentação de arquivos e diretórios.
 import os
-import shutil
+#import shutil
 
 
-DATALAKE_STAGE = 'DATALAKE_STAGE'  # Substitua pelo caminho do diretório de destino
+# Linha 9: Definir a constante 'DATALAKE_STAGE' como o caminho do diretório de destino no datalake.
+# Linha 10: Definir a constante 'DOWNLOADS' como o caminho para o diretório de downloads.
+DATALAKE_STAGE = 'DATALAKE_STAGE'
 DOWNLOADS = '/home/thiago/Downloads/'
 
 
+# Linha 19: Cria uma lista de nomes de arquivos para renomear. Isso é feito percorrendo os arquivos no diretório de downloads
+#           e selecionando apenas aqueles que terminam com a extensão ".crdownload".
+# Linha 20-22: Um loop 'for' é usado para iterar sobre cada nome de arquivo a ser renomeado.
+# Linha 21: Cria um novo nome de arquivo removendo a extensão ".crdownload" do nome original.
+# Linha 22: Usar a função 'os.rename' para renomear o arquivo. Isso é feito combinando o caminho completo do arquivo original
+#           com o novo nome. O arquivo é renomeado no mesmo diretório.
 files_to_rename = [arquivo for arquivo in os.listdir(DOWNLOADS) if arquivo.endswith(".crdownload")]
 for file_name in files_to_rename:
         new_name = file_name.replace(".crdownload", "")
         os.rename(os.path.join(DOWNLOADS, file_name), os.path.join(DOWNLOADS, new_name))
 
 
+# Linha 31: Cria uma lista de nomes de arquivos a serem movidos para o datalake. Isso é feito percorrendo os arquivos no diretório
+#           de downloads e selecionando apenas aqueles que terminam com a extensão ".gpx".
+# Linha 32-35: Um loop 'for' é usado para iterar sobre cada nome de arquivo a ser movido.
+# Linha 33-34: Constrói os caminhos de origem e destino dos arquivos usando o diretório de downloads e o diretório do datalake.
+# Linha 35: Usa a função 'os.rename' para mover o arquivo. O arquivo é movido da origem para o destino especificado.
+#           Isso também efetivamente renomeia o arquivo, uma vez que o diretório de destino é diferente do diretório de downloads.
 files_to_move = [files for files in os.listdir(DOWNLOADS) if files.endswith(".gpx")]
 for name_files in files_to_move:
     origin_path = os.path.join(DOWNLOADS, name_files)
     destiny_path = os.path.join(DATALAKE_STAGE, name_files)
     os.rename(origin_path, destiny_path)
-    print(f"Arquivo {name_files} movido para {destiny_path}")
--- WebScraping_OpenStreetMap.py
+++ WebScraping_OpenStreetMap.py
@@ -1,49 +1,49 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from time import sleep
-from bs4 import BeautifulSoup
-servico = Service(ChromeDriverManager().install())
+from selenium import webdriver # Importa o módulo do Selenium de automação de navegador.
+from webdriver_manager.chrome import ChromeDriverManager # Importa o gerenciador de driver para o Chrome.
+from selenium.webdriver.chrome.service import Service # Importa o serviço do driver do Chrome.
+from time import sleep # Importa a função sleep para pausar o código temporariamente.
+from bs4 import BeautifulSoup # Importa o Beautiful Soup para analisar HTML.
+servico = Service(ChromeDriverManager().install()) # Configura o serviço do driver do Chrome.
 
 
-URL_OPEN_STREET_MAP_TRACES = 'https://www.openstreetmap.org/traces' #--> Página do OpenstreetMap onde estão localizadas as rotas para download.
-PREFIX_URL_DOWNLOAD = 'https://www.openstreetmap.org' #--> Página do principal do OpenstreetMap. Esta variável será utilizada para montar a URL das páginas de download.
+URL_OPEN_STREET_MAP_TRACES = 'https://www.openstreetmap.org/traces' # [URL_OPEN_STREET_MAP_TRACES] = URL principal onde será feita a raspagem dos dados com o intuito de listar todas as rotas para download.
+PREFIX_URL_DOWNLOAD = 'https://www.openstreetmap.org' # [PREFIX_URL_DOWNLOAD] = URL que será usada como prefixo para montar as URLs das páginas de download.
+
+navegador = webdriver.Chrome(service=servico) # Inicializa o navegador Chrome usando o serviço configurado. 
+navegador.get(URL_OPEN_STREET_MAP_TRACES) # Acessa a página URL_OPEN_STREET_MAP_TRACES usando o navegador.
 
 
-navegador = webdriver.Chrome(service=servico)
-navegador.get(URL_OPEN_STREET_MAP_TRACES)
+page_content = navegador.page_source # Obtém o conteúdo HTML da página.
+site = BeautifulSoup(page_content, 'html.parser') # Cria um objeto BeautifulSoup a fim de analisar o conteúdo HTML obtido da página acima.
+routes = site.findAll('tr') # Encontra na na página todas as linhas das tabelas que possuem dados de rotas. Após essa localização as rotas localizadas são transformadas em uma lista.
 
+# list_pending_routes = [] # Inicializa uma lista para armazenar informações somente das rotas pendentes (Não utilizada por enquanto).
+# list_finished_routes = [] # Inicializa uma lista para armazenar informações somente das rotas finalizadas (Não utilizada por enquanto).
+list_routes = [] # Inicializa uma lista para armazenar informações de todas as rotas
 
-page_content = navegador.page_source
-site = BeautifulSoup(page_content, 'html.parser')
-routes = site.findAll('tr')
-
-
-list_pending_routes = []
-list_finished_routes = []
-list_routes = []
-
-
-for route in routes:
-    if route.find('span', attrs={'class': 'text-danger'}):
+for route in routes: # Itera pela lista das rotas localizadas através das linhas das tabelas da página.    
+    if route.find('span', attrs={'class': 'text-danger'}): # Se a rota estiver pendente, o link é localizado e adicionado à lista de rotas.
         pending_routes = route.find('span', attrs={'class': 'text-danger'})    
         pending_link_routes = route.find('a')        
-        list_routes.append([PREFIX_URL_DOWNLOAD+pending_link_routes['href']])
-        # list_pending_routes.append([PREFIX_URL_DOWNLOAD+pending_link_routes['href']])
-        print (f"{pending_routes.text}: {PREFIX_URL_DOWNLOAD+pending_link_routes['href']}")
-
-    else:         
+        list_routes.append([PREFIX_URL_DOWNLOAD+pending_link_routes['href']]) # Aqui é construída a URL final onde será possivel fazer o download do arquivo [.gpx]. Note que a URL de prefix é utilizada e o link é obtido através da tag 'a' e seu atributo 'href'.
+        # list_pending_routes.append([PREFIX_URL_DOWNLOAD+pending_link_routes['href']]) # Esse trecho constrói a URL para download apenas das rotas pendentes.
+    
+    else: # Se a rota estiver finalizada, encontrar o link e adicioná-lo à lista de rotas    
         finished_link_routes = route.find('a')        
-        list_routes.append([PREFIX_URL_DOWNLOAD+finished_link_routes['href']])
-        # list_finished_routes.append([PREFIX_URL_DOWNLOAD+finished_link_routes['href']])
-        print(f"FINISHED: {PREFIX_URL_DOWNLOAD+finished_link_routes['href']}")
+        list_routes.append([PREFIX_URL_DOWNLOAD+finished_link_routes['href']]) # Aqui é construída a URL final onde será possivel fazer o download do arquivo [.gpx]. Note que a URL de prefix é utilizada e o link é obtido através da tag 'a' e seu atributo 'href'.
+        # list_finished_routes.append([PREFIX_URL_DOWNLOAD+finished_link_routes['href']]) # Esse trecho constrói a URL para download apenas das rotas finalizadas.
+
+## Obs.: Quando usamos [requests.get] estamos usando o BeautifulSoup - e quando usamos o [.get] estamos usando o Selenium.
+
+for list_route in list_routes: # Itera pelas rotas na lista de rotas obtidas no for acima.
+    sleep(3) # Pausar por 3 segundos para que dê tempo do navegador renderizar a tela, a fim de se obter o link e o Selenium efetuar o clique, fazendo o download do arquivo.
+    url = list_route[0] # Obter a URL da página onde o link de download do arquivo está.
+    navegador.get(url)  # Acessa a URL com o Selenium #--> Exemplo onde usamos o Selenium somente com o [.get]
+    
+    # Exemplo onde usamos o Selenium para clicar em um elemento específico na página
+    navegador.find_element('xpath','//*[@id="content"]/div[2]/div/table/tbody/tr[1]/td/a').click() # Capturamos o 'xpath' do elemento que possui o link para download e efetuamos efetivamente o clique. O download do arquivo.gpx com o conteúdo da rota é feito na pasta Download.
+navegador.close() # Fecha o navegador após a conclusão das operações.
 
 
-## Quando usa [requests.get] estamos usando o BeautifulSoup - e quando usamos só o [.get] estamos usando o selenium
-for list_route in list_routes:
-    sleep(5)
-    url = list_route[0]
-    navegador.get(url)  #--> Exemplo onde usamos o Selenium somente com o [.get]
-    navegador.find_element('xpath','//*[@id="content"]/div[2]/div/table/tbody/tr[1]/td/a').click()
-navegador.close()
+
+
