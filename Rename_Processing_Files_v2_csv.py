0b5c683d10c867d13e05ed1323f3a5ac6bb0bdf0
Thiago Ribeiro da Silva <thiagor21@gmail.com>
2023-08-30 22:59:11 -0300

    Arquivos ajustados para que fiquem com todas as variáveis 100% em pt-br - Criado novo arquivo responsável por transformar os arquivos .csv da camada bronze -> adicionado o id da rota, nome do usuário, Feita a separação de data e hora em colunas distintas, Exclusão da coluna time-point.

--- Rename_Processing_Files_v2_csv.py
+++ Rename_Processing_Files_v2_csv.py
@@ -15,8 +15,8 @@ from minio.error import S3Error #--> O módulo S3Error é uma exceção específ
 ##############################
 ### DEFINIÇÃO DE VARIÁVEIS ### 
 ##############################
-PRE_PROCESSING = '/home/thiago/tcc_ufrj/PRE_PROCESSING' #--> PRE_PROCESSING recebe o caminho do diretório com os arquivos pré processados
-BRONZE_LAYER = 'bronze' #--> BRONZE_LAYER recebe uma string que representa uma camada ("layer") no servidor MinIO.
+PRE_PROCESSAMENTO = '/home/thiago/tcc_ufrj/PRE_PROCESSAMENTO' #--> PRE_PROCESSAMENTO recebe o caminho do diretório com os arquivos pré processados
+CAMADA_BRONZE = 'bronze' #--> CAMADA_BRONZE recebe uma string que representa uma camada ("layer") no servidor MinIO.
 
 ##############################################
 ### CRIANDO UMA INSTÂNCIA DO CLIENTE MINIO ###
@@ -29,43 +29,43 @@ minioclient = Minio('localhost:9000', #--> O cliente é configurado para se cone
 ###########################################################
 ### CONVERTENDO OS ARQUIVOS COM A EXTENSÃO .GPX EM .CSV ###
 ###########################################################
-files_to_pre_processing = [arquivo for arquivo in os.listdir(PRE_PROCESSING) if arquivo.endswith(".gpx")] #--> Listando todos os arquivos da pasta pré-processamento com extensão .gpx
-for file_to_pre_processing in files_to_pre_processing: #--> Iterando sobre a lista encontrada
-    file_path = os.path.join(PRE_PROCESSING, file_to_pre_processing) #--> Criando o caminho completo para o arquivo .gpx
+arquivos_pre_processamento = [arquivo for arquivo in os.listdir(PRE_PROCESSAMENTO) if arquivo.endswith(".gpx")] #--> Listando todos os arquivos da pasta pré-processamento com extensão .gpx
+for arquivo_pre_processamento in arquivos_pre_processamento: #--> Iterando sobre a lista encontrada
+    caminho_arquivo = os.path.join(PRE_PROCESSAMENTO, arquivo_pre_processamento) #--> Criando o caminho completo para o arquivo .gpx
     try:
-        with open(file_path, 'r', encoding='utf-8') as gpx_file: #--> Abrindo o arquivo .gpx
-            gpx = gpxpy.parse(gpx_file)
+        with open(caminho_arquivo, 'r', encoding='utf-8') as arquivo_gpx: #--> Abrindo o arquivo .gpx
+            gpx = gpxpy.parse(arquivo_gpx)
 
         info_rota = [] #--> Lista que servirá de apoio para converter o arquivo .gpx em uma lista
-        for track in gpx.tracks: #--> Sequência com aninhamentos de for para acessar as camadas internas do arquivos tais como do arquivo .gpx tais como: [tracks] [segments] e [points] e assim conseguir converter em uma lista.
-            for segment in track.segments:
-                for point in segment.points:
+        for trilha in gpx.tracks: #--> Sequência com aninhamentos de for para acessar as camadas internas do arquivos tais como do arquivo .gpx tais como: [tracks] [segments] e [points] e assim conseguir converter em uma lista.
+            for segmento in trilha.segments:
+                for ponto in segmento.points:
                     info_rota.append({
-                        'latitude': point.latitude, #--> Obtendo a latitude e atribuindo como item [value] da [Key] latitude da lista [info_rota]
-                        'longitude': point.longitude, #--> Obtendo a longitude e atribuindo como item [value] da [Key] longitude da lista [info_rota]
-                        'elevacao' : point.elevation, #--> Obtendo a elevação e atribuindo como item [value] da [Key] elevação da lista [info_rota]
-                        'time_point' : point.time #--> Obtendo a hora do ponto de latitude - longitude e elevação e atribuindo como item [value] da [Key] time_point da lista [info_rota]
+                        'latitude': ponto.latitude, #--> Obtendo a latitude e atribuindo como item [value] da [Key] latitude da lista [info_rota]
+                        'longitude': ponto.longitude, #--> Obtendo a longitude e atribuindo como item [value] da [Key] longitude da lista [info_rota]
+                        'elevacao' : ponto.elevation, #--> Obtendo a elevação e atribuindo como item [value] da [Key] elevação da lista [info_rota]
+                        'time_point' : ponto.time #--> Obtendo a hora do ponto de latitude - longitude e elevação e atribuindo como item [value] da [Key] time_point da lista [info_rota]
                     })
-        file_to_pre_processing_csv = file_to_pre_processing.replace('.gpx','.csv') #--> Ajustando o nome do arquivo para ficar com a extensão .csv ao inves de .gpx
+        arquivo_pre_processamento_csv = arquivo_pre_processamento.replace('.gpx','.csv') #--> Ajustando o nome do arquivo para ficar com a extensão .csv ao inves de .gpx
         info_rota_df = pd.DataFrame(info_rota) #--> Criando um dataframe a partir da lista 
-        info_rota_df.to_csv(f'{PRE_PROCESSING}/{file_to_pre_processing_csv}', index=False) #--> Criando um .csv a partir do DF criado acima e com o nome definido na variável anterior
-        os.remove(file_path) #--> Excluindo o arquivo .gpx da pasta PRE_PROCESSING
+        info_rota_df.to_csv(f'{PRE_PROCESSAMENTO}/{arquivo_pre_processamento_csv}', index=False) #--> Criando um .csv a partir do DF criado acima e com o nome definido na variável anterior
+        os.remove(caminho_arquivo) #--> Excluindo o arquivo .gpx da pasta PRE_PROCESSAMENTO
     except Exception as e: #--> Capturando qualquer erro que porventura ocorra
-        print(f"Erro ao converter o '{file_to_pre_processing}': {e}.") #--> Exibindo o erro
-        os.remove(file_path)
-        print(f"Excluindo o arquivo defeituoso: {file_to_pre_processing}")
+        print(f"Erro ao converter o '{arquivo_pre_processamento}': {e}.") #--> Exibindo o erro
+        os.remove(caminho_arquivo)
+        print(f"Excluindo o arquivo defeituoso: {arquivo_pre_processamento}")
         
 
 ################################################################################################################
-### MOVENDO OS ARQUIVOS COM A EXTENSÃO .CSV DA PASTA PRE_PROCESSING PARA A PRIMEIRA CAMADA (BRONZE) NO MINIO ###
+### MOVENDO OS ARQUIVOS COM A EXTENSÃO .CSV DA PASTA PRE_PROCESSAMENTO PARA A PRIMEIRA CAMADA (BRONZE) NO MINIO ###
 ################################################################################################################
-files_to_datalake = [files for files in os.listdir(PRE_PROCESSING) if files.endswith(".csv")] #--> Listando todos os arquivos da pasta pré-processamento com extensão .csv
-for name_file in files_to_datalake: #--> Iterando sobre cada item da lista
-    local_path = os.path.join(PRE_PROCESSING, name_file) #--> Criando o caminho completo para o arquivo .csv
-    if os.path.isfile(local_path): #--> Verificando se o caminho especificado está apontando para um arquivo válido no sistema de arquivos.
+arquivos_para_datalake = [arquivo for arquivo in os.listdir(PRE_PROCESSAMENTO) if arquivo.endswith(".csv")] #--> Listando todos os arquivos da pasta pré-processamento com extensão .csv
+for nome_arquivo in arquivos_para_datalake: #--> Iterando sobre cada item da lista
+    caminho_pre_proc = os.path.join(PRE_PROCESSAMENTO, nome_arquivo) #--> Criando o caminho completo para o arquivo .csv
+    if os.path.isfile(caminho_pre_proc): #--> Verificando se o caminho especificado está apontando para um arquivo válido no sistema de arquivos.
         try:
-            minioclient.fput_object(BRONZE_LAYER, name_file, local_path) #--> Usando o cliente Minio para enviar o arquivo para o bucket especificado (BRONZE_LAYER)
-            print(f"Arquivo {name_file} enviado com sucesso para o bucket.") #--> Exibindo a mensagem de sucesso após o upload dos arquivos para o bucket.
-            os.remove(local_path) # --> Após o envio bem sucedido para o bucket o arquivo é excluído da pasta PRE_PROCESSING
+            minioclient.fput_object(CAMADA_BRONZE, nome_arquivo, caminho_pre_proc) #--> Usando o cliente Minio para enviar o arquivo da pasta de pré processamento para o bucket especificado (CAMADA_BRONZE)
+            print(f"Arquivo {nome_arquivo} enviado com sucesso para o bucket.") #--> Exibindo a mensagem de sucesso após o upload dos arquivos para o bucket.
+            os.remove(caminho_pre_proc) # --> Após o envio bem sucedido para o bucket o arquivo é excluído da pasta PRE_PROCESSAMENTO
         except S3Error as e: #--> Capturando qualquer erro que porventura ocorra
-            print(f"Erro ao enviar o arquivo: {name_file} -> Erro: {e}") #--> Exibindo o erro
+            print(f"Erro ao enviar o arquivo: {nome_arquivo} -> Erro: {e}") #--> Exibindo o erro
